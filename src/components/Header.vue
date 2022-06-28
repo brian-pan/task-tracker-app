e8ee7ed0ece12c139e3022d3c2d30839d2a7c672
norazheng <zhengnuo@gmail.com>
2022-06-27 21:35:28 -0400

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+  <header>
+    <h1>Task Tracker</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  components: {},
+};
+</script>
+
+<style scoped>
+/* scoped means style only apply to this component */
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+</style>
