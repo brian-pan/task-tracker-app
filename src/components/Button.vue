d2f71b5d7c1e7d97d4269bd7680e7cdd10add598
brian pan <brianpanzifan@gmail.com>
2022-06-28 16:55:30 -0400

    save progress

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,13 +1,22 @@
 <template>
-  <button class="btn">{{ text }}</button>
+  <button @click="onClick()" :style="{ background: color }" class="btn">
+    {{ text }}
+  </button>
+  <!-- instead of using shortcut, full sentences are: -->
+  <!-- v-on:click="onClick()" -->
+  <!-- v-bind:style="{background: color}" -->
 </template>
 
 <script>
 export default {
   name: "Button",
   props: {
-    text: {
-      type: String,
+    text: String,
+    color: String,
+  },
+  methods: {
+    onClick() {
+      console.log("click");
     },
   },
 };
